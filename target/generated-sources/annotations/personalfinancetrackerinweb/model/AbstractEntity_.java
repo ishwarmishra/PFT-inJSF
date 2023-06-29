7586e6d111834339827d184e1ba2d849180e13ad
ishwarmishra <thisismeipm123@gmail.com>
2023-06-29 17:39:51 +0545

    deleteIncome UI complete

--- target/generated-sources/annotations/personalfinancetrackerinweb/model/AbstractEntity_.java
+++ target/generated-sources/annotations/personalfinancetrackerinweb/model/AbstractEntity_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2023-06-29T17:12:19")
+@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2023-06-29T17:33:14")
 @StaticMetamodel(AbstractEntity.class)
 public abstract class AbstractEntity_ { 
 
