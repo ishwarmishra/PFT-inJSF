d54d46d2ea989eab60754206781a34cee07ace4f
ishwarmishra <thisismeipm123@gmail.com>
2023-06-29 20:26:39 +0545

    commit changed

--- target/generated-sources/annotations/personalfinancetrackerinweb/model/AbstractEntity_.java
+++ target/generated-sources/annotations/personalfinancetrackerinweb/model/AbstractEntity_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2023-06-29T17:33:14")
+@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2023-06-29T18:07:16")
 @StaticMetamodel(AbstractEntity.class)
 public abstract class AbstractEntity_ { 
 
