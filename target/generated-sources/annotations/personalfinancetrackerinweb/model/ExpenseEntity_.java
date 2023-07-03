13d536bc5e2b060776131892643297aaecab344c
ishwarmishra <thisismeipm123@gmail.com>
2023-07-03 20:17:48 +0545

    UI  certain level

--- target/generated-sources/annotations/personalfinancetrackerinweb/model/ExpenseEntity_.java
+++ target/generated-sources/annotations/personalfinancetrackerinweb/model/ExpenseEntity_.java
@@ -6,7 +6,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2023-07-03T17:16:33")
+@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2023-07-03T19:50:59")
 @StaticMetamodel(ExpenseEntity.class)
 public class ExpenseEntity_ extends AbstractEntity_ {
 
