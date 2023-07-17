d18d97122f0dcd70aede6d44521ac87130fc7a96
ishwarmishra <thisismeipm123@gmail.com>
2023-07-17 13:13:40 +0545

    CONFIRMATION CODE FOR DELETE IS ADDED

--- target/generated-sources/annotations/personalfinancetrackerinweb/model/Category_.java
+++ target/generated-sources/annotations/personalfinancetrackerinweb/model/Category_.java
@@ -5,7 +5,7 @@ import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 import personalfinancetrackerinweb.model.CategoryType;
 
-@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2023-07-17T12:29:18")
+@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2023-07-17T12:52:11")
 @StaticMetamodel(Category.class)
 public class Category_ extends AbstractEntity_ {
 
--- target/generated-sources/annotations/personalfinancetrackerinweb/model/Income_.java
+++ target/generated-sources/annotations/personalfinancetrackerinweb/model/Income_.java
@@ -7,7 +7,7 @@ import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 import personalfinancetrackerinweb.model.Category;
 
-@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2023-07-17T12:29:18")
+@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2023-07-17T12:52:11")
 @StaticMetamodel(Income.class)
 public class Income_ extends AbstractEntity_ {
 
